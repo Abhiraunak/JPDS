79d6a6e2c2d4a2e6ae022c8d653715b40e9ea09b
Abhishek <abhishekkp1721@hotmail.com>
2025-07-11 18:29:51 +0530

    add project pages

---
+++ app/grouphousing/page.tsx
@@ -0,0 +1,72 @@
+import ImageTitle from "@/components/ImageTitle";
+import ProjectCard from "@/components/ProjectShowCase";
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <>
+            <main className="relative w-full h-screen selection:none">
+                <ImageTitle
+                    imageUrl="/grouphousing/header.jpg"
+                    headerText="Group Housing, Bikaner"
+                />
+            </main>
+
+            <section className="relative bg-[#F7F4EDff] min-h-screen pt-12 pb-20 ">
+                <div className="flex flex-col lg:grid lg:grid-cols-2 gap-10 lg:gap-20 px-4 md:px-8">
+                    {/* Image Column */}
+                    <div className="flex justify-center lg:justify-end lg:items-center lg:pl-5 h-[50vh] lg:h-full">
+                        <div className="relative w-full max-w-xl lg:w-[700px] h-full">
+                            <Image
+                                src={"/grouphousing/header.jpg"}
+                                alt="project image"
+                                fill
+                                className="rounded-md object-cover"
+                            />
+                        </div>
+                    </div>
+
+                    {/* Text Column */}
+                    <div className="text-black lg:pl-5 px-4 md:px-0 flex flex-col justify-center max-w-3xl mx-auto lg:max-w-none">
+                        <h1 className="font-sans font-semibold text-4xl md:text-5xl lg:text-6xl tracking-widest text-center select-none">
+                            Group Housing
+                        </h1>
+
+                        <h2 className="font-italianno pt-6 lg:pt-10 text-2xl md:text-3xl lg:text-4xl text-center select-none">
+                         Bikaner is in Rajasthan and known for its rich heritage architecture as well as hot climate.
+                        </h2>
+
+                        <p className="font-serif tracking-normal pt-6 lg:pt-10 text-base md:text-lg text-left select-none">
+                          The Scope of our work was to landscape the Space available for public access in a way that it serves all the age-groups.
+                          Challenge was the fact that this available space was triangular and thus, we needed to reduce the visual weight of it in the site.
+                          To manage it and distribute the visual weight thus, we divided the area into smaller rectangular areas and assigned 
+                          functional characters to them, by converting some to lawns and seating areas and turned the remaining pockets in to pergolas, 
+                          play areas and added water features into some. Then the corner areas were filled with trees and sports practice nets. 
+                          Most Plantations are local and rest are palms to create vegetative contrast.
+                        </p>
+
+                        <div className="pt-5">
+                            <h1 className="font-sans font-medium border-l-2 pl-4 text-lg md:text-xl">
+                                Bikaner
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="flex flex-col items-center gap-5 bg-[#F7F4EDff] px-4 pb-20">
+                <ProjectCard imageUrl="/grouphousing/housing1.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing2.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing3.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing4.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing5.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing6.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing7.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing8.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing9.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing10.jpg" title="" />
+                <ProjectCard imageUrl="/grouphousing/housing11.jpg" title="" />
+            </section>
+        </>
+    )
+}
---
+++ app/naturopathy/page.tsx
@@ -0,0 +1,69 @@
+import ImageTitle from "@/components/ImageTitle";
+import ProjectCard from "@/components/ProjectShowCase";
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <>
+            <main className="relative w-full h-screen selection:none">
+                <ImageTitle
+                    imageUrl="/naturopathy/header.jpg"
+                    headerText="Naturopathy Centre, Ranchi"
+                />
+            </main>
+
+            <section className="relative bg-[#F7F4EDff] min-h-screen pt-12 pb-20 ">
+                <div className="flex flex-col lg:grid lg:grid-cols-2 gap-10 lg:gap-20 px-4 md:px-8">
+                    {/* Image Column */}
+                    <div className="flex justify-center lg:justify-end lg:items-center lg:pl-5 h-[50vh] lg:h-full">
+                        <div className="relative w-full max-w-xl lg:w-[700px] h-full">
+                            <Image
+                                src={"/naturopathy/header.jpg"}
+                                alt="project image"
+                                fill
+                                className="rounded-md object-cover"
+                            />
+                        </div>
+                    </div>
+
+                    {/* Text Column */}
+                    <div className="text-black lg:pl-5 px-4 md:px-0 flex flex-col justify-center max-w-3xl mx-auto lg:max-w-none">
+                        <h1 className="font-sans font-semibold text-4xl md:text-5xl lg:text-6xl tracking-widest text-center select-none">
+                            Naturopathy Centre
+                        </h1>
+
+                        <h2 className="font-italianno pt-6 lg:pt-10 text-2xl md:text-3xl lg:text-4xl text-center select-none">
+                          Naturopathy Centre of Ranchi housed spaces for Yoga, Meditation and Spa.
+                        </h2>
+
+                        <p className="font-serif tracking-normal pt-6 lg:pt-10 text-base md:text-lg text-left select-none">
+                           It was mainly a Set of Courtyard and thus the challenge was to make the occupants feel connected to surrounding exteriors.
+                           By virtue of Façade Treatment and Fenestration development as well as Landscaping in the periphery 
+                           we achieved the same. To Visually connect the people inside to outside, we used glazing and connected 
+                           the central courtyard with the smaller subordinate ones.
+                        </p>
+
+                        <div className="pt-5">
+                            <h1 className="font-sans font-medium border-l-2 pl-4 text-lg md:text-xl">
+                                Ranchi
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="flex flex-col items-center gap-5 bg-[#F7F4EDff] px-4 pb-20">
+                <ProjectCard imageUrl="/naturopathy/center1.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center2.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center3.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center4.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center5.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center6.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center7.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center8.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center9.jpg" title="" />
+                <ProjectCard imageUrl="/naturopathy/center10.jpg" title="" />
+            </section>
+        </>
+    )
+}
